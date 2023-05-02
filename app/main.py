213f3457ddc0213618013b33bf44690479a0d30b
Ilia Timofeev <ilia.timofeev@gmail.com>
2023-05-02 22:41:59 +0000

    Fix tests and api

--- app/main.py
+++ app/main.py
@@ -6,18 +6,18 @@ import os
 
 # from dotenv import load_dotenv, find_dotenv
 from fastapi import Body, FastAPI
-from starlette.middleware.cors import CORSMiddleware
-from starlette.responses import RedirectResponse
-import uvicorn
 
+# from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+from app.api.api_v1.api import api_router
+from app.core.config import settings
 
 app = FastAPI(
-    title="leif",
-    version="1.0",
-    description="Search engine",
+    title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
 @app.get("/", include_in_schema=False)
 def docs_redirect():
-    return RedirectResponse(f"/docs")
+    return RedirectResponse("/docs")
