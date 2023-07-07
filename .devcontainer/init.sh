2390669dedd340d0d621ea0c0f8cef2230884030
Ilia Timofeev <ilia.timofeev@gmail.com>
2023-07-07 15:21:13 +0000

    auto start redis + tools

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,3 +1,3 @@
+git config --global core.editor "code --wait"
 pipx install poetry
 poetry config virtualenvs.create false && poetry install --with=dev
-docker-compose -f docker/docker-local-redis.yml up -d
